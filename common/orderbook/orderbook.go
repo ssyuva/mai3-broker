5ea90775aa1cf1619e70388a046645a4761004bd
renpu-mcarlo <renpu@mcarlo.com>
2021-01-20 02:32:40 +0800

    fix orderbook

--- common/orderbook/orderbook.go
+++ common/orderbook/orderbook.go
@@ -226,8 +226,10 @@ func (book *Orderbook) ChangeOrder(order *MemoryOrder, changeAmount decimal.Deci
 	defer book.lock.Unlock()
 
 	var tree *llrb.LLRB
+	isBuy := true
 	if order.Amount.IsNegative() {
 		tree = book.asksTree
+		isBuy = false
 	} else {
 		tree = book.bidsTree
 	}
@@ -248,7 +250,7 @@ func (book *Orderbook) ChangeOrder(order *MemoryOrder, changeAmount decimal.Deci
 	}
 	if price.Len() <= 0 {
 		tree.Delete(price)
-		if order.Amount.IsNegative() {
+		if !isBuy {
 			delete(book.askPrices, order.Price.String())
 		} else {
 			delete(book.bidPrices, order.Price.String())
