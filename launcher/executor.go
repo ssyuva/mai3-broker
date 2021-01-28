ec9920eed69b1e4788c3840d32a6d8b29e553a6c
renpu-mcarlo <renpu@mcarlo.com>
2021-01-28 23:07:32 +0800

    add log

--- launcher/executor.go
+++ launcher/executor.go
@@ -150,6 +150,7 @@ func (s *Executor) prepare(tx *model.LaunchTransaction) error {
 
 	limit := conf.Conf.GasLimit
 	tx.GasLimit = &limit
+	logger.Infof("111111111111111111 gasLimit:%d", *tx.GasLimit)
 	price := s.gasMonitor.GetGasPrice() * 1e9
 	tx.GasPrice = &price
 	return nil
@@ -174,6 +175,7 @@ func (s *Executor) send(tx *model.LaunchTransaction) error {
 			if err := dao.CreateTx(tx); err != nil {
 				return errors.Wrap(err, "create resend transaction failed")
 			}
+			logger.Infof("111111111111111111 gasLimit after create:%d", *tx.GasLimit)
 		} else {
 			logger.Infof("update tx, tx: %s", currHash)
 			if err := dao.UpdateTx(tx); err != nil {
