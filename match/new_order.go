f666ccf1ea0bf42ea8aad0fd1b5d6fc58bd795b8
renpu-mcarlo <renpu@mcarlo.com>
2021-04-16 17:15:04 +0800

    add log

--- match/new_order.go
+++ match/new_order.go
@@ -2,6 +2,7 @@ package match
 
 import (
 	"context"
+	"time"
 
 	"github.com/mcarloai/mai-v3-broker/common/mai3"
 	"github.com/mcarloai/mai-v3-broker/common/mai3/utils"
@@ -19,21 +20,26 @@ func (m *match) NewOrder(order *model.Order) string {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 
+	now := time.Now()
 	account, err := m.chainCli.GetAccountStorage(m.ctx, conf.Conf.ReaderAddress, m.perpetual.PerpetualIndex, m.perpetual.LiquidityPoolAddress, order.TraderAddress)
 	if err != nil {
 		logger.Errorf("new order:GetAccountStorage err:%s", err)
 		return model.MatchInternalErrorID
 	}
 
+	logger.Infof("GetAccountStorage 1111111: used:%d", time.Since(now).Milliseconds())
+
 	if !m.CheckCloseOnly(account, order) {
 		return model.MatchCloseOnlyErrorID
 	}
 
+	now = time.Now()
 	poolStorage, err := m.chainCli.GetLiquidityPoolStorage(m.ctx, conf.Conf.ReaderAddress, m.perpetual.LiquidityPoolAddress)
 	if err != nil {
 		logger.Errorf("new order: GetLiquidityPoolStorage fail! err:%s", err.Error())
 		return model.MatchInternalErrorID
 	}
+	logger.Infof("GetLiquidityPoolStorage 1111111: used:%d", time.Since(now).Milliseconds())
 
 	perpetual, ok := poolStorage.Perpetuals[m.perpetual.PerpetualIndex]
 	if !ok || !perpetual.IsNormal {
@@ -41,9 +47,11 @@ func (m *match) NewOrder(order *model.Order) string {
 		return model.MatchInternalErrorID
 	}
 
+	now = time.Now()
 	if !m.CheckOrderMargin(poolStorage, account, order, order.AvailableAmount) {
 		return model.MatchInsufficientBalanceErrorID
 	}
+	logger.Infof("CheckOrderMargin 1111111: used:%d", time.Since(now).Milliseconds())
 
 	activeOrders, err := m.dao.QueryOrder(order.TraderAddress, order.LiquidityPoolAddress, order.PerpetualIndex, []model.OrderStatus{model.OrderPending}, 0, 0, 0)
 	if err != nil {
