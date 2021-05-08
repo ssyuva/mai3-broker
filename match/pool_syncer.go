e8cbdd65cb2de4e1dfd19a8d390069ad848bfd4f
renpu-mcarlo <renpu@mcarlo.com>
2021-05-08 10:29:31 +0800

    update

--- match/pool_syncer.go
+++ match/pool_syncer.go
@@ -14,18 +14,18 @@ import (
 
 const POOL_STORAGE_USE_DURATION = 60
 
-type poolStatus struct {
+type poolInfo struct {
+	storage   *model.LiquidityPoolStorage
 	latestGet time.Time
 	isDirty   bool
 }
 
 type poolSyncer struct {
-	ctx          context.Context
-	mu           sync.RWMutex
-	pools        []string
-	poolStatuses map[string]*poolStatus
-	poolStorage  map[string]*model.LiquidityPoolStorage
-	chainCli     chain.ChainClient
+	ctx         context.Context
+	mu          sync.RWMutex
+	pools       []string
+	poolStorage map[string]*poolInfo
+	chainCli    chain.ChainClient
 }
 
 func newPoolSyncer(ctx context.Context, cli chain.ChainClient) *poolSyncer {
@@ -33,7 +33,7 @@ func newPoolSyncer(ctx context.Context, cli chain.ChainClient) *poolSyncer {
 		ctx:         ctx,
 		chainCli:    cli,
 		pools:       make([]string, 0),
-		poolStorage: make(map[string]*model.LiquidityPoolStorage),
+		poolStorage: make(map[string]*poolInfo),
 	}
 }
 
@@ -59,11 +59,11 @@ func (p *poolSyncer) runSyncer() error {
 func (p *poolSyncer) syncPool(pool string) {
 	p.mu.Lock()
 	defer p.mu.Unlock()
-	status, ok := p.poolStatuses[pool]
+	info, ok := p.poolStorage[pool]
 	// pool storage not be used in duration, do not refresh it until someone used it again
-	if ok && time.Since(status.latestGet).Seconds() > POOL_STORAGE_USE_DURATION {
-		if !status.isDirty {
-			status.isDirty = true
+	if ok && time.Since(info.latestGet).Seconds() > POOL_STORAGE_USE_DURATION {
+		if !info.isDirty {
+			info.isDirty = true
 		}
 		return
 	}
@@ -73,9 +73,9 @@ func (p *poolSyncer) syncPool(pool string) {
 		logger.Errorf("Pool Syncer: GetLiquidityPoolStorage fail! pool:%s err:%v", pool, err)
 		p.poolStorage[pool] = nil
 	}
-	p.poolStorage[pool] = poolStorage
-	if status.isDirty {
-		status.isDirty = false
+	info.storage = poolStorage
+	if info.isDirty {
+		info.isDirty = false
 	}
 }
 
@@ -90,27 +90,24 @@ func (p *poolSyncer) AddPool(pool string) {
 	if poolStorage == nil || err != nil {
 		logger.Errorf("Pool Syncer: GetLiquidityPoolStorage fail! pool:%s err:%v", pool, err)
 	}
-	p.poolStorage[pool] = poolStorage
-	p.poolStatuses[pool] = &poolStatus{
+	p.poolStorage[pool] = &poolInfo{
 		latestGet: time.Now(),
 		isDirty:   false,
+		storage:   poolStorage,
 	}
 }
 
 func (p *poolSyncer) GetPoolStorage(pool string) *model.LiquidityPoolStorage {
 	p.mu.RLock()
 	defer p.mu.RUnlock()
-	status, ok := p.poolStatuses[pool]
+	info, ok := p.poolStorage[pool]
 	if !ok {
 		return nil
 	}
-	if status.isDirty {
-		status.latestGet = time.Now()
+	if info.isDirty {
+		// update latestGet, syncer will update PoolStorage in next round
+		info.latestGet = time.Now()
 		return nil
 	}
-	storage, ok := p.poolStorage[pool]
-	if !ok {
-		return nil
-	}
-	return storage
+	return info.storage
 }
